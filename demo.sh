e741b777a7d1ceaed5db0dec7eddd2bf982c7e76
ElvisNB <167739302+ElvisNB@users.noreply.github.com>
2024-09-18 00:29:56 -0700

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-# 定义一个数组，包含要删除的用户列表
-USERS=(test1 test2 test3 test4 test5)
-
-# 循环删除用户
-for USERNAME in "${USERS[@]}"
+# 循环创建用户 test1 到 test5
+for i in {1..5}
 do
-  echo "Starting to remove user $USERNAME..."
+  USERNAME="test${i}"
 
-  # 删除用户及其主目录和邮件目录
-  # -r 选项用于删除用户的主目录和邮件目录
-  # -f 选项用于强制删除，即使用户当前已登录
-  sudo userdel -rf "$USERNAME"
+  # 创建用户
+  sudo useradd "$USERNAME"
 
-  # 检查用户是否成功删除
-  if [ $? -eq 0 ]; then
-    echo "User $USERNAME has been removed."
-  else
-    echo "Failed to remove user $USERNAME"
-  fi
+  # 输出提示信息
+  echo "User $USERNAME has been created."
+
+  # （可选）为每个用户设置密码
+  echo "Setting a password for the user $USERNAME"
+  echo "$USERNAME:password" | sudo chpasswd
+
+  # （可选）创建用户的主目录
+  sudo mkdir -p "/home/$USERNAME"
+  sudo chown "$USERNAME":"$USERNAME" "/home/$USERNAME"
+
+  # （可选）为用户设置默认shell
+  sudo usermod -s /bin/bash "$USERNAME"
 done
 
-echo "All specified users have been removed."
+echo "All users have been created."
